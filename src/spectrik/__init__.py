b95ed0ffe610768c1a3d6207b71d8a9f8444fd30
jheddings <jason@heddings.com>
2026-02-18 20:00:50 -0700

    feat: re-export ProjectLoader and Workspace from package root

--- src/spectrik/__init__.py
+++ src/spectrik/__init__.py
@@ -2,6 +2,7 @@
 
 from spectrik.blueprints import Blueprint as Blueprint
 from spectrik.context import Context as Context
+from spectrik.hcl import ProjectLoader as ProjectLoader
 from spectrik.projects import Project as Project
 from spectrik.specs import Absent as Absent
 from spectrik.specs import Ensure as Ensure
@@ -9,3 +10,4 @@ from spectrik.specs import Present as Present
 from spectrik.specs import Specification as Specification
 from spectrik.specs import SpecOp as SpecOp
 from spectrik.specs import spec as spec
+from spectrik.workspace import Workspace as Workspace
